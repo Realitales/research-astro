fb656f07c4896f6198fe2509f4f6f14d7dba06c4
Jay Anthon Rico <jayanthon.rico@fligno.com>
2023-05-09 13:17:10 +0800

    remove test autocomplete of aliases

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
-export default function Card({ message }) {
+export const Card = ({ message }: any) => {
   return <div className="font-oswald">{message}</div>;
-}
+};
+
+export default Card;
