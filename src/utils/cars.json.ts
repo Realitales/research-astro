f1da906eb9b9250e574a3770516c8faa0179ee31
Jay Anthon Rico <jayanthon.rico@fligno.com>
2023-05-10 15:12:04 +0800

    added mock data for prod test

---
+++ src/utils/cars.json.ts
@@ -0,0 +1,24 @@
+import type Cars from "src/types/Cars";
+
+const cars: Cars[] = [
+  {
+    id: "1001",
+    name: "Mercedez",
+    model: "X1001",
+    year: "2021",
+  },
+  {
+    id: "1002",
+    name: "Mitsubishi",
+    model: "RO20",
+    year: "2019",
+  },
+  {
+    id: "1003",
+    name: "Ferrari",
+    model: "Z1",
+    year: "2023",
+  },
+];
+
+export default cars;
